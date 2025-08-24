3839a5b69fc42fc8e6b8642b0ed851a857fe5920
tpankrah <tpankrah@live.com>
2025-08-24 08:45:59 +0000

    boskstore assignment -- caps order details, checker not validating

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
