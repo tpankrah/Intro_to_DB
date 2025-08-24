669e5443d3cf605d3d80a59cfaee9d374651c823
tpankrah <tpankrah@live.com>
2025-08-24 08:38:44 +0000

    boskstore assignment -- removed backticks, checker not validating

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (`customer_id`) REFERENCES Customers(`customer_id`)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 -- Order_Details: Stores information about the books included in each order.
 
