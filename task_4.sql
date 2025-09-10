588e307bbf87ab13c8cc1adbf72955957a34a6e1
Theophilus Ankrah <tpankkrah@live.com>
2025-09-10 12:32:42 +0000

    task 4

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,3 @@
--- Write a script that prints the full
--- description of the table books from the 
--- database alx_book_store in your MySQL server.
-
--- The database name will be passed as an argument of the 
--- mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
--- The name of the file should be task_4.sql
--- All SQL keywords should be in uppercase
--- Repo:
-
--- GitHub repository: Intro_to_DB
--- File: task_4.sql
 
 
 SELECT 
