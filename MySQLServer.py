b6034592ecd30b71b7d9e2f62035567b9907be0e
Theophilus Ankrah <tpankkrah@live.com>
2025-09-10 15:39:36 +0000

    python db script

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,7 +3,7 @@
 
 # Name of python script should be MySQLServer.py
 # If the database alx_book_store already exists, your script should not fail
-# You are not allowed to use the SELECT or SHOW statements
+# You are not allowed to use the 
 # NOTE :
 
 # Required to print message such as Database 'alx_book_store' created successfully! 
